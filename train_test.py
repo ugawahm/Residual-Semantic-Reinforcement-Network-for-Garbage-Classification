6d8ed79c25039c0b79d528fbd579440e7a8bd071
ugawahm <53989001+ugawahm@users.noreply.github.com>
2023-03-19 14:39:46 +0800

    Update train_test.py

--- train_test.py
+++ train_test.py
@@ -7,17 +7,11 @@ import torch.nn as nn
 import torch.optim as optim
 import matplotlib
 from matplotlib import pyplot as plt
-# from prettytable import PrettyTable
-# from torch.optim import lr_scheduler
 
-# import timm
-# import timm.optim
-# import timm.scheduler
 
 from torchvision import transforms, datasets
 from tqdm import tqdm
-from VCRNet_CBAM_1_19 import resnext50_32x4d
-# from test_train import *
+from model import vcrnet50
 matplotlib.use('TkAgg')
 
 
@@ -27,10 +21,8 @@ def main():
 
     data_transform = {
         "train": transforms.Compose([
-                                    # transforms.RandomRotation(30, expand=False, center=None, fill=None),
+                                    transforms.RandomRotation(30, expand=False, center=None, fill=None),
                                     # 随机旋转
-                                    # transforms.RandomGrayscale(p=0.1),  # 0.1概率变成灰度
-                                    # transforms.ColorJitter(brightness=0.5, contrast=0.5, hue=0.5),#随机光学变换
                                     transforms.RandomResizedCrop(224),
                                     transforms.RandomHorizontalFlip(),
                                     transforms.ToTensor(),
@@ -41,9 +33,7 @@ def main():
                                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
 
     data_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))  # get data root path
-    # image_path = os.path.join(data_root, "data_set", "laji_data")
-    image_path = os.path.join(data_root, "PycharmProjects", "laji12", "garbage_classification")  # laji data set path
-    # image_path = os.path.join(data_root, "PycharmProjects", "laji12", "garbage_new")  # laji data set path
+    image_path = os.path.join(data_root, "PycharmProjects", "garbage_classification")  #  dataset path
     assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
     train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),
                                          transform=data_transform["train"])
@@ -74,25 +64,9 @@ def main():
     print("using {} images for training, {} images for validation.".format(train_num,
                                                                            val_num))
 
-    # net = Resnext(50, 5)
-    net = resnext50_32x4d(num_classes=12)  # 为迁移学习时（）内为空
-    # --------------------------迁移学习分割线----------------------------------
-    # load pretrain weights
-    # download url: https://download.pytorch.org/models/resnet34-333f7ec4.pth
-    # 使用迁移学习则启用下面代码
-    #
-    # net = resnext50_32x4d()
-    # model_weight_path = "weight_document/resnext50_32x4d-7cdf4587.pth"
-    # assert os.path.exists(model_weight_path), "file {} does not exist.".format(model_weight_path)
-    # net.load_state_dict(torch.load(model_weight_path, map_location='cpu'), strict=False)    # , strict=False
-    # # for param in net.parameters():
-    # #     param.requires_grad = False.0
-    #
-    # # change fc layer structure
-    # in_channel = net.fc.in_features
-    # net.fc = nn.Linear(in_channel, 12)                   # 分类数
 
-    # -------------------------迁移学习分割线-----------------------------------
+    net = vcrnet50(num_classes=12) 
+
     net.to(device)
     # define loss function
     loss_function = nn.CrossEntropyLoss()
@@ -100,22 +74,14 @@ def main():
     # construct an optimizer
     params = [p for p in net.parameters() if p.requires_grad]
     optimizer = optim.Adam(params, lr=0.0001)
-    # lrs = []
 
-    # （余弦模拟退火 + warm up 动态调整学习率） + SGD优化器
-    # optimizer = optim.SGD(params, lr=0.1, momentum=0.9, weight_decay=0.0001)
-    # scheduler = timm.scheduler.CosineLRScheduler(optimizer=optimizer,
-    #                                              t_initial=150,
-    #                                              lr_min=0.0001,
-    #                                              warmup_t=4,
-    #                                              warmup_lr_init=0.0001)
 
     Loss_list = []
     Accuracy_list = []
 
     epochs = 150
     best_acc = 0.0
-    save_path = 'weight_document/vcr_0318_12.pth'
+    save_path = 'weight_document/vcrnet.pth'
     train_steps = len(train_loader)
     val_steps = len(validate_loader)
     for epoch in range(epochs):
@@ -139,7 +105,6 @@ def main():
                                                                      epochs,
                                                                      loss)
         # print("epoch: %d, lr：%f" % (epoch, optimizer.param_groups[0]['lr']))
-        # scheduler.step(epoch)  # 动态更新学习率
 
         # validate
         net.eval()
@@ -191,7 +156,7 @@ def main():
     # plt.ylabel('Val loss')
     plt.xlabel('迭代次数')
     plt.ylabel('验证损失')
-    plt.savefig('./results/vcr318_p12.png', dpi=300)
+    plt.savefig('./results/1.png', dpi=300)
     plt.show()
 
     print('Finished Training')
